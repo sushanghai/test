670f86102e9705b794bf67b45ffbe2dc832c5ee9
Tiger <sushanghai@qq.com>
2019-03-16 07:39:22 +0800

    v1
    v1

--- es_oracle/esServer.py
+++
@@ -1,8 +0,0 @@
-from xmlrpc.server import SimpleXMLRPCServer
-def ServerA():
-    servver = SimpleXMLRPCServer(("localhost",9200),allow_none=True)
-    servver.register_function(hello())
-def hello():
-    print("hello")
-
-ServerA()
